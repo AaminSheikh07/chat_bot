b4245da5da7527628c66f60dc93210629c533834
AaminSheikh07 <137299437+AaminSheikh07@users.noreply.github.com>
2024-06-03 12:01:39 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Login from "./Components/Login";
+import '..\\node_modules\\bootstrap\\dist\\css\\bootstrap.min.css'
+
+const App = () => {
+  return (
+    <div>
+      <Login></Login>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Login.jsx
@@ -0,0 +1,103 @@
+import "../CSS/Login.css";
+// import 'node_modules\bootstrap\dist\css\bootstrap.min.css'
+
+export default function Login() {
+  return (
+    <>
+      <nav class="navbar bg-body-tertiary">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            <img
+              src="/docs/5.3/assets/brand/bootstrap-logo.svg"
+              alt="Bootstrap"
+              width="30"
+              height="24"
+            />
+          </a>
+        </div>
+      </nav>
+      
+      <body>
+        <section>
+          {" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>
+          <div class="signin">
+            <div class="content">
+              <h2>Sign In</h2>
+
+              <div class="form">
+                <div class="inputBox">
+                  <input type="text" required /> <i>Username</i>
+                </div>
+
+                <div class="inputBox">
+                  <input type="password" required /> <i>Password</i>
+                </div>
+
+                <div class="links">
+                  {" "}
+                  <a href="#">Forgot Password</a> <a href="#">Signup</a>
+                </div>
+
+                <div class="inputBox">
+                  <input type="submit" value="Login" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </body>
+    </>
+  );
+}
